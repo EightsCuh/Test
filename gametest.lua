bac5ab44028d8eb1a7013274fd5fabe92f901007
Eights <34287554+EightsCuh@users.noreply.github.com>
2021-07-26 20:06:42 -0700

    Update gametest.lua

--- gametest.lua
+++ gametest.lua
@@ -1,9 +1,9 @@
-local placeholder = ''
+
 
 if game.PlaceId == 6554116553 then
     print(game.PlaceId)
-    placeholder = 'Game'
+   local placeholder = 'Game'
 elseif game.PlaceId == 4042427666 then
     print("Arf")
-    placeholder  = 'Arf'
+   local placeholder  = 'Arf'
 end
